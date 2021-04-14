4663c77cb5790bdd03c57998e5859ce9616ef706
Thomas <10285945+thomaseb191@users.noreply.github.com>
2021-04-14 15:24:58 -0400

    Implement naive color reduction

--- src/reductions/naive.go
+++ src/reductions/naive.go
@@ -3,15 +3,45 @@ package reductions
 import (
 	g "../graphs"
 	"fmt"
-	"time"
 )
 
+// RunNaive
+/* Implements Naive Color Reduction Alg found here:
+https://stanford.edu/~rezab/classes/cme323/S16/projects_reports/bae.pdf
+poolSize = 4 means there are 5 colors nodes and be colored as [0,1,2,3,4]
+ */
 func RunNaive(gr g.Graph, poolSize int, debug int) g.Graph {
 	if debug % 2 == 1 {
 		fmt.Printf("Starting reduction for %s algorithm...\n", "Naive")
 	}
-	time.Sleep(100 * time.Millisecond)
-	//TODO: IMPLEMENT REDUCTION
+
+	size := len(gr.Nodes)
+
+	for i := poolSize+1; i < size; i++ {
+		color := MinColor(*gr.Nodes[i], poolSize)
+		if color == -1 {
+			fmt.Errorf("MinColor() did not return a valid value")
+		}
+		gr.Nodes[i].Color = color
+	}
 
 	return gr
+}
+
+func MinColor(n g.Node, poolSize int) int {
+	for i := 0; i < poolSize; i++ {
+		contained := false
+		for _, neighbor := range n.Neighbors {
+			if neighbor.Color == i {
+				contained = true
+				break
+			}
+		}
+		if contained {
+			continue
+		}
+		return i
+	}
+
+	return -1
 }
