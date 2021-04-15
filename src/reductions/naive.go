4a42feb62595bfaed67549ef91d3b81c98e97214
Thomas <10285945+thomaseb191@users.noreply.github.com>
2021-04-14 20:40:58 -0400

    Fix poolSize confusion

--- src/reductions/naive.go
+++ src/reductions/naive.go
@@ -8,7 +8,7 @@ import (
 // RunNaive
 /* Implements Naive Color Reduction Alg found here:
 https://stanford.edu/~rezab/classes/cme323/S16/projects_reports/bae.pdf
-poolSize = 4 means there are 5 colors nodes and be colored as [0,1,2,3,4]
+MaxDegree 4 means there are 5 colors nodes can be colored as [0,1,2,3,4]
  */
 func RunNaive(gr g.Graph, poolSize int, debug int) g.Graph {
 	if debug % 2 == 1 {
@@ -17,8 +17,8 @@ func RunNaive(gr g.Graph, poolSize int, debug int) g.Graph {
 
 	size := len(gr.Nodes)
 
-	for i := poolSize+1; i < size; i++ {
-		color := MinColor(*gr.Nodes[i], poolSize)
+	for i := gr.MaxDegree+1; i < size; i++ {
+		color := MinColor(*gr.Nodes[i], gr.MaxDegree)
 		if color == -1 {
 			fmt.Errorf("MinColor() did not return a valid value")
 		}
