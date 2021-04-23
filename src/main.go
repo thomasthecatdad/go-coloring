f3ad3a61cf6f789ad5763df38d8232f3f931f232
jmj51@duke.edu <jmj51@duke.edu>
2021-04-23 00:14:31 -0400

    Fix displaying graphs and make more tests/graphs

--- src/main.go
+++ src/main.go
@@ -34,20 +34,20 @@ func main() {
 		testDirectives := t.ParseTestFile(testFileName)
 
 		for _, td := range testDirectives {
-			runTestAndPrintResult(td)
+			runTestAndPrintResult(td, testFileName)
 		}
 	} else if len(inputArgs) >= 3 && len(inputArgs) <= 5 {
 		// Run a singular test
 		td := t.ParseArgsList(os.Args[1:])
 
-		runTestAndPrintResult(td)
+		runTestAndPrintResult(td, td.GraphFile)
 	} else {
 		log.Fatal("Incorrect arguments specified")
 	}
 }
 
 // runTestAndPrintResults is a helper method to run a specific test set
-func runTestAndPrintResult(td t.TestDirective) {
+func runTestAndPrintResult(td t.TestDirective, testName string) {
 	tResults := t.RunTest(td.GraphFile, td.Algos, td.PoolSize, td.Debug)
 	for _, k := range tResults {
 		//TODO: REFINE TEST OUTPUT, MAYBE EXPORT RESULTS TO FILE
